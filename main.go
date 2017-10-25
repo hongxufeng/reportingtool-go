8f7d6c12a871fd7f26cdcbe171ad008c358bc14a
W <331805370@qq.com>
2017-10-25 16:09:41 +0800

    增加第三方路由器denco

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package reportingtool
 
 import (
-	"encoding/xml"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"model"
+	"github.com/naoina/denco"
+	"log"
+	"net/http"
 )
 
 func main()  {
-
+	mux := denco.NewMux()
+	handler, err := mux.Build([]denco.Handler{
+		mux.GET("/", Index),
+	})
+	if err != nil {
+		panic(err)
+	}
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
