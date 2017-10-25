a2658f2ee1a14001816c94c9e73354a8ebd12a6c
W <331805370@qq.com>
2017-10-25 17:55:29 +0800

    http框架完成

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
-package reportingtool
+package main
 
 import (
 	"github.com/naoina/denco"
 	"log"
 	"net/http"
+	"fmt"
 )
 
-func main()  {
+func main() {
 	mux := denco.NewMux()
 	handler, err := mux.Build([]denco.Handler{
-		mux.GET("/", Index),
+		mux.GET("/ReportingTool", Index),
+		mux.GET("/ReportingTool/:style", Style),
 	})
 	if err != nil {
 		panic(err)
 	}
 	log.Fatal(http.ListenAndServe(":8080", handler))
+}
+
+func Index(w http.ResponseWriter, r *http.Request, params denco.Params) {
+	fmt.Fprintf(w, "Welcome to /ReportingTool!\n")
+}
+
+func Style(w http.ResponseWriter, r *http.Request, params denco.Params) {
+	fmt.Fprintf(w, "您要创建 %s!\n", params.Get("style"))
 }
