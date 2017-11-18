ea688e22681c521446c1cd78e611a825fb155373
W <331805370@qq.com>
2017-11-18 18:12:12 +0800

    update数据库表改动

--- sql/W_User_List.sql
+++ sql/W_User_List.sql
@@ -6,17 +6,19 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `w_user_list`;
 CREATE TABLE `w_user_list` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户自增uid',
-  `username` varchar(255) NOT NULL DEFAULT '' COMMENT '用户名，登陆可用',
-  `password` varchar(255) NOT NULL COMMENT '经salt加密过的密码',
-  `salt` smallint(6) NOT NULL,
-  `state` bit(4) NOT NULL COMMENT '用户状态',
+  `uid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` char(50) NOT NULL DEFAULT '' COMMENT '用户名，登陆可用',
+  `password` char(50) NOT NULL COMMENT '经salt加密过的密码',
+  `nickname` varchar(25) DEFAULT NULL,
+  `salt` int(6) NOT NULL,
+  `state` bit(1) NOT NULL COMMENT '用户状态',
   `avatar` varchar(255) DEFAULT NULL COMMENT '用户头像地址',
-  `user_agent` varchar(255) DEFAULT NULL COMMENT '用户登录的user_agent',
+  `user_agent` varchar(255) NOT NULL COMMENT '用户登录的user_agent',
   `time_create` datetime DEFAULT NULL COMMENT '注册时间',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='W_后台登录用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='W_后台登录用户表';
 
 -- ----------------------------
 -- Records of w_user_list
 -- ----------------------------
+INSERT INTO `w_user_list` VALUES ('1', 'moon', '1f0c428a6bfe454bb1115805cc0cdaaa', '月儿', '211314', '', null, 'Mozilla', null);
